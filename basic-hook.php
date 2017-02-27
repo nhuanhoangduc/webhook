9491816f0d8332a3e6e8bf37e0433485d99406af
hoang duc nhuan <nhuan.hoangduc@outlook.com>
2017-02-27 11:35:14 +0700

    access token

--- basic-hook.php
+++ basic-hook.php
@@ -1,7 +1,7 @@
 <?php 
 
 header('Content-Type: text/html; charset=utf-8');
-define('VERIFY_TOKEN', 'EAAFyda9ohaQBAE5Vg5QqssTL9CYwzQwvQFz0t2Xh5tyG6ZAHjzgZBEiroXpKhvXELBq6WXmkuiYbLiKMLDX4JdjCQQIaNL385guwVUO5wWTwR5PmSPdZBZBgKlQie4Q7uxLUycqosXvZCIHaxGNsZCnPuD6EZBIsZB3nx0hIE82gMQZDZD'); 
+define('VERIFY_TOKEN', 'EAACEdEose0cBAPPXjXynBofruE7ZBNWZAt4PWdCyv4JTg9USewn0J232iQi6gqBfL2iZC3jmkCQymnJl4tQtZBDJfKRSaizfcfIRaBQ7OE4J5ftREkbE5ZCgvLfBIG38bbN5QbBLlxaJCOfEpS6FyIPi2QNsk9BuxMb4xwwSYjLzC4ofZAQJkoZASiX4FjrRZAYZD'); 
 $method = $_SERVER['REQUEST_METHOD']; 
 if ($method == 'GET' && $_GET['hub_mode'] == 'subscribe' && $_GET['hub_verify_token'] == VERIFY_TOKEN) {        
     echo $_GET['hub_challenge'];        
